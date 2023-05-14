8d9e9c23f0c98561d213833e8b86304fde1830d8
Matthew Sheets <msheets@pymtpos.com>
2023-05-14 08:02:55 -0500

    init

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,22 +6,22 @@ type Props = {
   title?: string;
 };
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
+const Layout = ({ children, title = "Coming Soon | DVC Closings" }: Props) => (
   <div className="relative min-h-screen flex flex-col justify-center items-center bg-hero bg-cover">
     <Head>
-      <title>{title} | Themeptation</title>
+      <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta
         name="description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
+        content="Coming Soon! | For now please vising https://dvcclosings.com"
       />
-      <meta property="og:title" content={`${title} | Themeptation`} />
+      <meta property="og:title" content={`${title}`} />
       <meta
         property="og:description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
+        content="Coming Soon! | For now please vising https://dvcclosings.com"
       />
-      <meta property="og:url" content="https://slioth.themepttation.net/" />
+      <meta property="og:url" content="https://dvcclosings.com" />
       <meta property="og:type" content="website" />
     </Head>
     {children}
